212a6437cfb210e88852728503a561a27ccce5e9
Dominic Nunez <dominicnunez@gmail.com>
2023-10-18 11:21:54 -0500

    tasks 1-4 complete

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,23 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
-
+  const dropdown = document.getElementById('citySelect')
+  dropdown.addEventListener('change', async () => {
+    // let formattedCity = dropdown.value.replace(" ", "+")
+    dropdown.disabled = true;
+    const info = document.querySelector('.info')
+    info.textContent = "Fetching weather data..."
+    const baseURL = "http://localhost:3003/api/weather?city="
+    let selectedCity = dropdown.value
+    let cityURL = baseURL + selectedCity
+    
+    try {
+      const response = await axios.get(cityURL);
+      console.log("Reponse Data: ", response.data)
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+    })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 }
